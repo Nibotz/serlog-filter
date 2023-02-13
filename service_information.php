38436cca714f0ed258a4c6b4e73feffe8244827b
Niilo Mikkola <niilosmikkola@gmail.com>
2023-02-13 22:25:29 +0200

    pages working with url.

--- service_information.php
+++ service_information.php
@@ -7,12 +7,17 @@ include("db_connection.php");
 $conn = openConn("serlog");
 
 // get the data for the tab from database
-$query = "SELECT * FROM services WHERE id=" . $_POST["id"] . ";";
+$query = "SELECT * FROM services WHERE id IN (" . $_POST["ids"] . ");";
 
 $tab_info = $conn->query($query);
-$tab_info = $tab_info->fetch_assoc();
+
+// loop trough the data
+$output = [];
+while ($row = $tab_info->fetch_assoc()) {
+    $output[] = $row;
+}
 
 // return the data
-exit(json_encode($tab_info));
+exit(json_encode($output));
 
 ?>
